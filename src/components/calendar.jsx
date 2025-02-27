13be2ebcd6d2d82cc2af1fb9236b70bb4b61d2a1
anilthomas14 <anil@codzoc.com>
2025-02-27 20:08:28 +0530

    big calendar component added

--- src/components/calendar.jsx
+++ src/components/calendar.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from 'react-redux';
 import {Calendar as BigCalendar, momentLocalizer} from "react-big-calendar";
 import moment from "moment";
+// import Dialog from "material-ui/Dialog";
+// import FlatButton from "material-ui/FlatButton";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import { selectDateStart, selectDateSuccess, selectDateFailure } from '../redux/calendarSlice';
 
@@ -12,17 +14,41 @@ const Calendar = () => {
   const dispatch = useDispatch();
 
   const { data, isSelected, selectedDate, isLoading } = useSelector((state) => state.calendar);
+  const [currentDate, setCurrentDate] = useState(new Date());
+  const [currentView, setCurrentView] = useState("month");
 
   const dateArr = Object.keys(data);
 
   const handleSelect = (dateInfo) => {
     const date = new Date(dateInfo.start)
     .toLocaleDateString("en-GB")
-    .replace(/\//g, "-");
+    .replace(/\//g, "-")
 
     dispatch(selectDateSuccess({ data, selectedDate: date }));
   }
 
+  const dayPropGetter = (date) => {
+    const formattedDate = moment(date).format("DD-MM-YYYY");
+
+    if (selectedDate === formattedDate) {
+      return {
+        style: {
+          backgroundColor: "rgb(106 132 249)",
+          color: "#000",
+          borderRadius: "5px",
+        },
+      };
+    }
+    return {};
+  };
+
+  const handleNavigate = (newDate) => {
+    setCurrentDate(newDate);
+  };
+
+  const handleViewChange = (newView) => {
+    setCurrentView(newView);
+  };
 
   return (
     <>
@@ -34,10 +60,14 @@ const Calendar = () => {
         timeslots={2}
         defaultView="month"
         defaultDate={new Date()}
+        date={currentDate}
+        view={currentView}
+        onNavigate={handleNavigate}
+        onView={handleViewChange}
         selectable={true}
-        // onSelectEvent={() => {}}
         onSelectSlot={handleSelect}
         style={{ height: '500px' }}
+        dayPropGetter={dayPropGetter}
       />
     
     </>
